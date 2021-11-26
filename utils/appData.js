5dc4ce7d556643b9676d448a2410ec338247a982
Parag Katoch <paragkatoch5@gmail.com>
2021-11-26 18:23:46 +0530

    added domains data

--- utils/appData.js
+++ utils/appData.js
@@ -36,6 +36,7 @@ import AdviceMisc from "../public/assets/advice_misc.svg";
 import GrowthMisc from "../public/assets/growth_misc.svg";
 import AppMisc from "../public/assets/app_misc.svg";
 
+// App Data
 export const APP = {
 	NAME: "DEV-Portal",
 	EMAIL: "Kamra103@gmail.com",
@@ -113,45 +114,583 @@ export const DomainData = {
 	courses: [
 		{
 			img: WebDev,
-			alt: "web development",
+			alt: "web_development",
 			courseName: "Web Development",
-			link: "/resource/web_development",
+			link: "/resource/domains/web_development",
+			data: [
+				{
+					img: WebDev,
+					alt: "web_development",
+					heading: "Web Development",
+					nextAlt: "roadmap",
+					description:
+						"We've compiled a reference list, a roadmap to follow, the best project ideas, and some entertaining quizzes to test your knowledge for the Web domain.",
+				},
+				{
+					heading: "Roadmap",
+					alt: "roadmap",
+					description:
+						"After evaluating the tech stacks you'll need, build your skills from the ground up. These structured pathways will help you decide what to start first.",
+					dataCards: [
+						{
+							heading: "Frontend",
+							img: WebDev,
+							alt: "frontend",
+							link: "https://roadmap.sh/frontend",
+						},
+						{
+							heading: "Backend",
+							img: WebDev,
+							alt: "backend",
+							link: "https://roadmap.sh/backend",
+						},
+					],
+				},
+				{
+					heading: "Resources",
+					description:
+						"Time saver skill booster efficient video resources to enhance your knowledge. Esteemed and tested channels to start from very basics to advance level with super creativity level.",
+					alt: "resources",
+					dataCards: [
+						{
+							heading: "FreeCodeCamp",
+							img: WebDev,
+							alt: "freecodecamp",
+							link: "https://www.youtube.com/c/Freecodecamp",
+							type: "youtube",
+						},
+						{
+							heading: "Traversy Media",
+							img: WebDev,
+							alt: "traversy Media",
+							link: "https://www.youtube.com/c/TraversyMedia",
+							type: "youtube",
+						},
+						{
+							heading: "CS50",
+							img: WebDev,
+							alt: "CS50",
+							link: "https://www.youtube.com/c/cs50",
+							type: "youtube",
+						},
+						{
+							heading: "Fireship",
+							img: WebDev,
+							alt: "fireship",
+							link: "https://www.youtube.com/c/Fireship",
+							type: "youtube",
+						},
+						{
+							heading: "The Net Ninza",
+							img: WebDev,
+							alt: "the net ninza",
+							link: "https://www.youtube.com/c/TheNetNinja",
+							type: "youtube",
+						},
+						{
+							heading: "FreeCodeCamp",
+							img: WebDev,
+							alt: "freecodecamp",
+							link: "https://www.freecodecamp.org/",
+							type: "website",
+						},
+						{
+							heading: "MDN",
+							img: WebDev,
+							alt: "MDN",
+							link: "https://developer.mozilla.org/en-US/",
+							type: "website",
+						},
+						{
+							heading: "W3Schools",
+							img: WebDev,
+							alt: "w3schools",
+							link: "https://www.w3schools.com/",
+							type: "website",
+						},
+						{
+							heading: "Frontend Masters",
+							img: WebDev,
+							alt: "frontend Masters",
+							link: "https://frontendmasters.com/",
+							type: "website",
+						},
+						{
+							heading: "Educative",
+							img: WebDev,
+							alt: "educative",
+							link: "https://www.educative.io/",
+							type: "website",
+						},
+						{
+							heading: "Wes Bos",
+							img: WebDev,
+							alt: "wes bos",
+							link: "https://wesbos.com/",
+							type: "website",
+						},
+					],
+				},
+				{
+					heading: "Project Ideas",
+					description: "No Description",
+					alt: "project_ideas",
+					dataCards: [
+						{
+							heading: "Code Editor",
+							img: WebDev,
+							alt: "Online Code Editor (React)",
+							link: "https://www.crio.do/projects/react-code-editor/",
+						},
+						{
+							heading: "Codechef Notifier",
+							img: WebDev,
+							alt: "Codechef Notifier",
+							link: "https://www.crio.do/projects/javascript-codechef-notifier/",
+						},
+						{
+							heading: "Amazon clone",
+							img: WebDev,
+							alt: "Amazon clone using React",
+							link: "https://www.crio.do/projects/react-amazon-clone/",
+						},
+						{
+							heading: "Sorting Visualizer",
+							img: WebDev,
+							alt: "Sorting Visualizer",
+							link: "https://www.crio.do/projects/javascript-sorting-visualiser/",
+						},
+						{
+							heading: "Slack clone",
+							img: WebDev,
+							alt: "Slack clone",
+							link: "https://www.crio.do/projects/react-slack-clone/",
+						},
+						{
+							heading: "Connect4",
+							img: WebDev,
+							alt: "Connect4",
+							link: "https://www.crio.do/projects/python-multiplayer-game-connect4/",
+						},
+						{
+							heading: "Rat in a Maze",
+							img: WebDev,
+							alt: "Rat in a Maze",
+							link: "https://www.crio.do/projects/react-ratinamaze/",
+						},
+						{
+							heading: "FuzzyURLs",
+							img: WebDev,
+							alt: "FuzzyURLs",
+							link: "https://www.crio.do/projects/python-django-url-shortener/",
+						},
+					],
+				},
+				{
+					heading: "Test your knowledge",
+					alt: "test",
+					description:
+						"Challenges make success simpler, try out these step-guided assessments. Test your skills to look out for your mistakes and be the better version of yourself",
+					dataCards: [
+						{
+							heading: "FreeCodeCamp",
+							img: WebDev,
+							alt: "freecodecamp",
+							link: "https://www.freecodecamp.org/",
+						},
+						{
+							heading: "Frontend Mentor",
+							img: WebDev,
+							alt: "Frontend Mentor",
+							link: "https://www.frontendmentor.io/",
+						},
+						{
+							heading: "100DaysCSS",
+							img: WebDev,
+							alt: "100DaysCSS",
+							link: "https://100dayscss.com/",
+						},
+						{
+							heading: "CSSBattle",
+							img: WebDev,
+							alt: "CSSBattle",
+							link: "https://cssbattle.dev/",
+						},
+					],
+				},
+			],
 		},
 		{
 			img: AndroidDev,
-			alt: "android development",
+			alt: "android_development",
 			courseName: "Android Development",
-			link: "/resource/android_development",
+			link: "/resource/domains/android_development",
+			data: [
+				{
+					img: AndroidDev,
+					alt: "android_development",
+					heading: "Android Development",
+					nextAlt: "roadmap",
+					description:
+						"We've compiled a reference list, a roadmap to follow, the best project ideas, and some entertaining quizzes to test your knowledge for the Android domain.",
+				},
+				{
+					heading: "Roadmap",
+					alt: "roadmap",
+					description:
+						"After evaluating the tech stacks you'll need, build your skills from the ground up. These structured pathways will help you decide what to start first.",
+					dataCards: [
+						{
+							heading: "Basic",
+							img: AndroidDev,
+							alt: "basic",
+							link: "https://roadmap.sh/android",
+						},
+						{
+							heading: "Detailed",
+							img: AndroidDev,
+							alt: "detailed",
+							link: "https://www.androidelements.com/",
+						},
+					],
+				},
+				{
+					heading: "Resources",
+					description:
+						"Time saver skill booster efficient video resources to enhance your knowledge. Esteemed and tested channels to start from very basics to advance level with super creativity level.",
+					alt: "resources",
+					dataCards: [
+						{
+							heading: "PhilippLackner",
+							img: AndroidDev,
+							alt: "PhilippLackner",
+							link: "https://www.youtube.com/c/PhilippLackner/featured",
+							type: "youtube",
+						},
+						{
+							heading: "tutorialsEU",
+							img: AndroidDev,
+							alt: "tutorialsEU",
+							link: "https://www.youtube.com/channel/UCGjv_3tbzJ8yKuvacoqmO-Q",
+							type: "youtube",
+						},
+						{
+							heading: "Rahul Pandey",
+							img: AndroidDev,
+							alt: "Rahul Pandey",
+							link: "https://www.youtube.com/c/RahulPandeyrkp",
+							type: "youtube",
+						},
+						{
+							heading: "Freecodecamp",
+							img: AndroidDev,
+							alt: "Freecodecamp",
+							link: "https://www.youtube.com/c/Freecodecamp",
+							type: "youtube",
+						},
+						{
+							heading: "CodeWithHarry",
+							img: AndroidDev,
+							alt: "CodeWithHarry",
+							link: "https://www.youtube.com/channel/UCeVMnSShP_Iviwkknt83cww",
+							type: "youtube",
+						},
+						{
+							heading: "Neat Roots",
+							img: AndroidDev,
+							alt: "Neat Roots",
+							link: "https://www.youtube.com/channel/UCyVD1TyR6Me7Yl7C2F5DMYA",
+							type: "Androidsite",
+						},
+						{
+							heading: "Journal Dev",
+							img: AndroidDev,
+							alt: "Journal Dev",
+							link: "https://www.journaldev.com/",
+							type: "website",
+						},
+						{
+							heading: "Codepath",
+							img: AndroidDev,
+							alt: "codepath",
+							link: "https://guides.codepath.com/android",
+							type: "website",
+						},
+						{
+							heading: "Frontend Masters",
+							img: AndroidDev,
+							alt: "frontend Masters",
+							link: "",
+							type: "website",
+						},
+						{
+							heading: "Kotlin",
+							img: AndroidDev,
+							alt: "kotlin",
+							link: "https://kotlinlang.org/",
+							type: "website",
+						},
+						{
+							heading: "Typealias",
+							img: AndroidDev,
+							alt: "typealias",
+							link: "https://typealias.com/start/",
+							type: "website",
+						},
+						{
+							heading: "Data Flair",
+							img: AndroidDev,
+							alt: "Data Flair",
+							link: "https://data-flair.training/blogs/android-tutorials-home/",
+							type: "website",
+						},
+					],
+				},
+				{
+					heading: "Project Ideas",
+					alt: "project_ideas",
+					description: "No Description",
+					dataCards: [
+						{
+							heading: "Companion App",
+							img: AndroidDev,
+							alt: "Companion App",
+							link: "https://www.crio.do/projects/flutter-companion-app/",
+						},
+						{
+							heading: "Contact App",
+							img: AndroidDev,
+							alt: "Contact App",
+							link: "https://www.crio.do/projects/java-android-contact-app/",
+						},
+						{
+							heading: "Breakout Ball Game",
+							img: AndroidDev,
+							alt: "Breakout Ball Game",
+							link: "https://www.crio.do/projects/java-breakout-game/",
+						},
+						{
+							heading: "QuizUp",
+							img: AndroidDev,
+							alt: "QuizUp - A Quiz Application",
+							link: "https://www.crio.do/projects/android-java-quiz-application/",
+						},
+						{
+							heading: "Instagram Clone",
+							img: AndroidDev,
+							alt: "Instagram Clone",
+							link: "https://www.crio.do/projects/kotlin-android-instagram-clone/",
+						},
+						{
+							heading: "Notes & Password Manager",
+							img: AndroidDev,
+							alt: "Notes & Password Manager",
+							link: "https://www.crio.do/projects/java-android-notes-password-manager/",
+						},
+						{
+							heading: "Ecommerce platform",
+							img: AndroidDev,
+							alt: "A Hyperlocal Ecommerce platform",
+							link: "https://www.crio.do/projects/flutter-hyperlocal-ecommerce/",
+						},
+						{
+							heading: "bFit",
+							img: AndroidDev,
+							alt: "bFit-A Cognitive Game",
+							link: "https://www.crio.do/projects/java-android-game/",
+						},
+					],
+				},
+				{
+					heading: "Test your knowledge",
+					alt: "test",
+					description:
+						"Challenges make success simpler, try out these step-guided assessments. Test your skills to look out for your mistakes and be the better version of yourself",
+					dataCards: [
+						{
+							heading: "Tutorials Point",
+							img: AndroidDev,
+							alt: "Tutorials Point",
+							link: "https://www.tutorialspoint.com/android/android_online_test.htm",
+						},
+						{
+							heading: "WsCubetech",
+							img: AndroidDev,
+							alt: "WsCubetech",
+							link: "https://www.wscubetech.com/quiz-test-android",
+						},
+						{
+							heading: "Javatpoint",
+							img: AndroidDev,
+							alt: "javatpoint",
+							link: "https://www.javatpoint.com/android-quiz",
+						},
+						{
+							heading: "Google",
+							img: AndroidDev,
+							alt: "Google",
+							link: "https://developer.android.com/courses/quizzes/android-kotlin-fundamental",
+						},
+						{
+							heading: "Pro Android Dev",
+							img: AndroidDev,
+							alt: "Pro Android Dev",
+							link: "https://proandroiddev.com/android-quiz-1-1f475c7170fd",
+						},
+						{
+							heading: "Career Guru 99",
+							img: AndroidDev,
+							alt: "Career Guru 99",
+							link: "https://career.guru99.com/android-quiz/",
+						},
+					],
+				},
+			],
 		},
 		{
 			img: CloudDev,
-			alt: "cloud development",
+			alt: "cloud_development",
 			courseName: "Cloud Development",
-			link: "/resource/cloud_development",
+			link: "/resource/domains/cloud_development",
+			data: [
+				{
+					img: CloudDev,
+					alt: "cloud_development",
+					heading: "Cloud Development",
+					nextAlt: "roadmap",
+					description:
+						"We've compiled a reference list, a roadmap to follow, the best project ideas, and some entertaining quizzes to test your knowledge for the Android domain.",
+				},
+				{
+					heading: "Roadmap",
+					alt: "roadmap",
+					description:
+						"After evaluating the tech stacks you'll need, build your skills from the ground up. These structured pathways will help you decide what to start first.",
+					dataCards: [
+						{
+							heading: "Basic",
+							img: CloudDev,
+							alt: "basic",
+							link: "https://whimsical.com/cloud-support-associate-roadmap-S2unty6W9pqmMRGq77h2jk",
+						},
+						{
+							heading: "Detailed",
+							img: CloudDev,
+							alt: "detailed",
+							link: "https://www.notion.so/Getting-started-with-Cloud-ffbff1b34ca04038abb388a3e8936bb2",
+						},
+					],
+				},
+				{
+					heading: "Resources",
+					alt: "resources",
+					description:
+						"Time saver skill booster efficient video resources to enhance your knowledge. Esteemed and tested channels to start from very basics to advance level with super creativity level.",
+					dataCards: [
+						{
+							heading: "AWS",
+							img: CloudDev,
+							alt: "AWS",
+							link: "https://www.awseducate.com/registration",
+							type: "website",
+						},
+						{
+							heading: "Microsoft",
+							img: CloudDev,
+							alt: "Microsoft",
+							link: "https://docs.microsoft.com/en-ie/learn/",
+							type: "website",
+						},
+						{
+							heading: "AWS Reskill",
+							img: CloudDev,
+							alt: "AWS Reskill",
+							link: "https://awsreskill.com/",
+							type: "website",
+						},
+						{
+							heading: "Techgig",
+							img: CloudDev,
+							alt: "Techgig",
+							link: "https://www.techgig.com/googlecloud",
+							type: "website",
+						},
+						{
+							heading: "Azure",
+							img: CloudDev,
+							alt: "Azure",
+							link: "https://azure.microsoft.com/en-in/",
+							type: "website",
+						},
+						{
+							heading: "Coursera",
+							img: CloudDev,
+							alt: "Coursera",
+							link: "https://www.coursera.org/courses?query=gcp",
+							type: "website",
+						},
+					],
+				},
+				{
+					heading: "Test your knowledge",
+					alt: "test",
+					description:
+						"Challenges make success simpler, try out these step-guided assessments. Test your skills to look out for your mistakes and be the better version of yourself",
+					dataCards: [
+						{
+							heading: "Tech Gig",
+							img: CloudDev,
+							alt: "Tech Gig",
+							link: "https://www.techgig.com/googlecloud",
+						},
+						{
+							heading: "Qwiklabs",
+							img: CloudDev,
+							alt: "Qwiklabs",
+							link: "https://www.qwiklabs.com/",
+						},
+						{
+							heading: "Cloud Academy",
+							img: CloudDev,
+							alt: "Cloud Academy",
+							link: "https://cloudacademy.com/blog/test-your-cloud-knowledge-on-aws-azure-or-google-cloud-platform/",
+						},
+						{
+							heading: "KonfHub",
+							img: CloudDev,
+							alt: "KonfHub",
+							link: "https://quiz.konfhub.com/",
+						},
+					],
+				},
+			],
 		},
 		{
 			img: AIML,
-			alt: " artificial intelligence and machine learning",
+			alt: "ai_ml",
 			courseName: "AI/ML",
-			link: "/resource/ai_ml",
+			link: "/resource/domains/ai_ml",
 		},
 		{
 			img: IOSDev,
-			alt: "ios development",
+			alt: "ios_development",
 			courseName: "IOS Development",
-			link: "/developer/domain",
+			link: "/resource/domains/ios_development",
+
 			disabled: true,
 		},
 		{
 			img: ComingSoon,
-			alt: "coming soon",
+			alt: "coming_soon",
 			courseName: "More coming Soon....",
-			link: "/developer/domain",
+			link: "/resource/domains/coming_soon",
+
 			disabled: true,
 		},
 	],
 };
 
+// Core Data
 export const CoreData = {
 	heading: "Explore CORE of your interest",
 	className: "core",
@@ -226,6 +765,7 @@ export const CoreData = {
 	],
 };
 
+// Home Modules
 export const HomeModules = [
 	{
 		title: "Developer",
@@ -261,6 +801,7 @@ export const HomeModules = [
 	},
 ];
 
+// Developer Modules
 export const DeveloperModules = [
 	{
 		title: "Follow your domain",
@@ -296,6 +837,7 @@ export const DeveloperModules = [
 	},
 ];
 
+// Degree Modules
 export const DegreeModules = [
 	{
 		title: "Keep CORE in line",
@@ -323,6 +865,7 @@ export const DegreeModules = [
 	},
 ];
 
+// Misc Modules
 export const MiscModules = [
 	{
 		title: "Networking",
