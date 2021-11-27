3cb2d89150068106e14a033357d1ec4613101537
Parag Katoch <paragkatoch5@gmail.com>
2021-11-27 13:16:44 +0530

    changed meta tags

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -26,75 +26,21 @@ function MyApp({ Component, pageProps }) {
 					key="viewport"
 				/>
 
+				<meta
+					property="og:image"
+					content={`${process.env.PUBLIC_URL}/logo192.png`}
+					key="image"
+				/>
+
 				<link rel="manifest" href="/manifest.json" />
 				<link
-					rel="apple-touch-icon"
-					sizes="57x57"
-					href="/apple-icon-57x57.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="60x60"
-					href="/apple-icon-60x60.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="72x72"
-					href="/apple-icon-72x72.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="76x76"
-					href="/apple-icon-76x76.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="114x114"
-					href="/apple-icon-114x114.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="120x120"
-					href="/apple-icon-120x120.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="144x144"
-					href="/apple-icon-144x144.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="152x152"
-					href="/apple-icon-152x152.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-icon-180x180.png"
-				/>
-				<link
 					rel="icon"
-					type="image/png"
-					sizes="192x192"
-					href="/android-icon-192x192.png"
+					href={`${process.env.PUBLIC_URL}/favicon.ico`}
+					type="image/x-icon"
 				/>
 				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="96x96"
-					href="/favicon-96x96.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
+					rel="apple-touch-icon"
+					href={`${process.env.PUBLIC_URL}/logo192.png`}
 				/>
 			</Head>
 
