8c500397041537c3501f18ba1b90653c7eb56573
Parag Katoch <paragkatoch5@gmail.com>
2021-12-10 19:26:27 +0530

    added noopener and noreferrer in a tag in BasicButton

--- components/BasicButton/index.js
+++ components/BasicButton/index.js
@@ -1,11 +1,16 @@
-import styles from "./styles/basicButton.module.scss";
 import Link from "next/link";
+import styles from "./styles/basicButton.module.scss";
 
 export default function BasicButton(data) {
+	const https = data.url.includes("http");
+
 	return (
 		<button className={styles.card_button}>
 			<Link href={data.url}>
-				<a target={data.url.includes("http") ? "_blank" : "_self"}>
+				<a
+					target={https ? "_blank" : "_self"}
+					rel={https ? "noopener noreferrer" : ""}
+				>
 					{data.kickoff}
 				</a>
 			</Link>
